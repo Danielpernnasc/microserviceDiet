8303b0d56336705deaab78fde7e1e54fd120303b
Danielpernnasc <dpericles6@gmail.com>
2026-07-10 09:28:40 -0300

    first-commit

---
+++ src/test/java/com/trainday/microserviceDiet/MicroserviceDietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainday.microserviceDiet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
